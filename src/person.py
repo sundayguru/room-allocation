00bfefa1dfbeaa479829886769f0eb7bbc27d150
sunday Nwuguru <sunday.nwuguru@andela.com>
2016-04-10 22:43:34 +0100

    implemented person init method to pass edge cases test

--- src/person.py
+++ src/person.py
@@ -3,6 +3,12 @@ class Person(object):
 	is_allocated = False
 
 	def __init__(self,name,person_type,living_space = False):
+		if type(name) != str or type(person_type) != str or type(living_space) != bool:
+			raise ValueError
+
+		if person_type != 'FELLOW' and person_type != 'STAFF':
+			raise ValueError
+
 		self.name = name
 		self.person_type = person_type
 		self.living_space = living_space
