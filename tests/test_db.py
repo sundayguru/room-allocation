5aefb046f5646b034c89f9ea8a304e4d73c5aefb
sunday Nwuguru <sunday.nwuguru@andela.com>
2016-04-16 15:22:42 +0100

    added test case for db find method

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,10 +7,16 @@ from src.db import Db
 class TestDb(unittest.TestCase):
 	"""Test cases for Db class"""
 
+	def setUp(self):
+		self.db = Db()
+
 	def test_db_init_opens_connection(self):
-		db = Db()
-		self.assertEqual(type(db.connection), sqlite3.Connection)
-		db.find()
+		self.assertEqual(type(self.db.connection), sqlite3.Connection)
+
+	def test_db_find_returns_false_when_invalid_table(self):
+		data = self.db.find(1)
+		self.assertEqual(data, False)
+
 
 if __name__ == '__main__':
     unittest.main()
