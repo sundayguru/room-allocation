1969554d4256edb8a57f660f8da3ff5844b3482f
sunday Nwuguru <sunday.nwuguru@andela.com>
2016-04-23 19:49:33 +0100

    refactored person class and commented methods

--- src/person.py
+++ src/person.py
@@ -24,18 +24,21 @@ class Person(Db,FileMan):
 
 
 	def full_details(self):
+		"""returns person full details as string"""
 		details = self.name() + ' ' + self.person_type + ' ' + self.get_state_dict(self.living_space) 
 		details += ' ' + self.get_state_dict(self.is_allocated) + ' ' +self.date_time
 		return details
 
 
 	def get_details_dict(self):
+		"""returns person full details as dictionary"""
 		return {
 		'firstname':self.firstname,
 		'lastname':self.lastname,
-		'allocation':self.transalate(self.living_space),
+		'living_space':self.transalate(self.living_space),
 		'allocated':self.transalate(self.is_allocated),
 		'person_type':self.person_type,
+		'assigned_room':self.assigned_room,
 		'date_time':self.date_time,
 		}
 
@@ -47,15 +50,18 @@ class Person(Db,FileMan):
 			return self.state_dict[0]
 
 	def transalate(self,status):
+		"""this simply converts bool to int (0 or 1)"""
 		if status:
 			return 1
 		else:
 			return 0
 
 	def name(self):
+		"""returns full name of the person"""
 		return self.firstname + ' ' + self.lastname
 
 	def allocate(self):
+		"""allocates person to a room from pickle file room data"""
 		file = FileMan('rooms.pkl')
 		rooms = file.pickle_load()
 		if not rooms:
@@ -71,12 +77,6 @@ class Person(Db,FileMan):
 
 
 	def save(self):
-		data = {
-		'firstname':self.firstname,
-		'lastname':self.lastname,
-		'person_type':self.person_type,
-		'allocated':self.transalate(self.is_allocated),
-		'living_space':self.transalate(self.living_space),
-		'assigned_room':self.assigned_room,
-		}
+		"""Saves person into sqlite database"""
+		data = self.get_details_dict()
 		return self.create(data)
